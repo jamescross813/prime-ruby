09f022f477079f66d1873bd964495b7150449a18
jamescross <crossjames813@gmail.com>
2021-01-23 12:49:13 -0600

    commit tree 68c3102422b1ddfbe5934f45a0d78f6c5623b13f

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+require 'pry'
+
+def prime?(num)
+    if !num.integer?
+      false
+    elsif num < 2
+      false
+    #elsif num == 2
+    #  true
+    else
+         (2..num-1).to_a.all? {|int| num % int != 0}
+     end
+    
+end
+
+
+    
